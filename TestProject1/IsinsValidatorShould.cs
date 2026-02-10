441c3460ed3796a0f447648ca65a7e6506d7ff05
Kassio Santana <kciosantana@gmail.com>
2026-02-09 22:34:15 -0300

    #1 Pass_Validation_When_Security_Is_Valid

---
+++ TestProject1/IsinsValidatorShould.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Securities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1
+{
+    public class IsinsValidatorShould
+    {
+        private IsinsValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new IsinsValidator();
+        }
+
+        [Test]
+        public async Task Pass_Validation_When_Security_Is_Valid()
+        {
+            // Arrange
+            var request = new ExecuteSecurityRequest
+            {
+                Isins = new List<string> { "IT1234567890", "IT0987654321" }
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+        }
+    }
+}
